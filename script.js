17a8a79f89267434bf3ad59b401f875b5786977b
Nawar Abou Trabi <nawarmajedd@gmail.com>
2025-07-31 13:23:46 +0100

    Added personal website files

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('.links');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+
+    // Optional: Prevent scrolling when menu is open
+    if (nav.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = '';
+    }
+};
+
+fetch('footer.html')
+  .then(res => res.text())
+  .then(html => document.getElementById('footer').innerHTML = html);
